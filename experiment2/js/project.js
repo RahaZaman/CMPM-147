6a0aab5639bd306ae0d257bce75ff6d0f1c88f81
RahaZaman <rahamatzaman@gmail.com>
2024-04-17 00:23:45 -0700

    small updates

--- experiment2/js/project.js
+++ experiment2/js/project.js
@@ -1,6 +1,6 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Rahamat Zaman
+// Date: April 16 2024
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
@@ -23,12 +23,12 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+  // // create an instance of the class
+  // let myInstance = new MyProjectClass("value1", "value2");
 
-  // call a method on the instance
-  myInstance.myMethod();
+  // // call a method on the instance
+  // myInstance.myMethod();
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
