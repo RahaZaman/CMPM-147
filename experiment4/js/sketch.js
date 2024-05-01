ef4bd067dbdab9ed311e05fd9431df67a7b8d252
RahaZaman <rahamatzaman@gmail.com>
2024-05-01 01:05:32 -0700

    Adding code and summaries/descriptions

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,4 +1,5 @@
-// sketch.js - purpose and description here
+// sketch.js - creates three interactive infinite worlds where users can dynamically interact with grass, 
+// ocean life, and desert elements, each customizable using a world key.
 // Author: Rahamat Zaman
 // Date: April 30 2024
 
@@ -60,3 +61,184 @@ function mousePressed() {
     // code to run when mouse is pressed
 }
 
+// Grassy Infinite World Generator 
+const s1 = (a) => {
+  /* global p5 */
+  /* exported preload, setup, draw, mouseClicked */
+
+  // Project base code provided by {amsmith,ikarth}@ucsc.edu
+
+
+  let tile_width_step_main; // A width step is half a tile's width
+  let tile_height_step_main; // A height step is half a tile's height
+
+  // Global variables. These will mostly be overwritten in setup().
+  let tile_rows, tile_columns;
+  let camera_offset;
+  let camera_velocity;
+
+  /////////////////////////////
+  // Transforms between coordinate systems
+  // These are actually slightly weirder than in full 3d...
+  /////////////////////////////
+  function worldToScreen([world_x, world_y], [camera_x, camera_y]) {
+    let i = (world_x - world_y) * tile_width_step_main;
+    let j = (world_x + world_y) * tile_height_step_main;
+    return [i + camera_x, j + camera_y];
+  }
+
+  function worldToCamera([world_x, world_y], [camera_x, camera_y]) {
+    let i = (world_x - world_y) * tile_width_step_main;
+    let j = (world_x + world_y) * tile_height_step_main;
+    return [i, j];
+  }
+
+  function tileRenderingOrder(offset) {
+    return [offset[1] - offset[0], offset[0] + offset[1]];
+  }
+
+  function screenToWorld([screen_x, screen_y], [camera_x, camera_y]) {
+    screen_x -= camera_x;
+    screen_y -= camera_y;
+    screen_x /= tile_width_step_main * 2;
+    screen_y /= tile_height_step_main * 2;
+    screen_y += 0.5;
+    return [Math.floor(screen_y + screen_x), Math.floor(screen_y - screen_x)];
+  }
+
+  function cameraToWorldOffset([camera_x, camera_y]) {
+    let world_x = camera_x / (tile_width_step_main * 2);
+    let world_y = camera_y / (tile_height_step_main * 2);
+    return { x: Math.round(world_x), y: Math.round(world_y) };
+  }
+
+  function worldOffsetToCamera([world_x, world_y]) {
+    let camera_x = world_x * (tile_width_step_main * 2);
+    let camera_y = world_y * (tile_height_step_main * 2);
+    return new p5.Vector(camera_x, camera_y);
+  }
+}
+
+// Ocean - Sea Infinite World Generator
+const s2 = (a) => {
+  /* global p5 */
+  /* exported preload, setup, draw, mouseClicked */
+
+  // Project base code provided by {amsmith,ikarth}@ucsc.edu
+
+
+  let tile_width_step_main; // A width step is half a tile's width
+  let tile_height_step_main; // A height step is half a tile's height
+
+  // Global variables. These will mostly be overwritten in setup().
+  let tile_rows, tile_columns;
+  let camera_offset;
+  let camera_velocity;
+
+  /////////////////////////////
+  // Transforms between coordinate systems
+  // These are actually slightly weirder than in full 3d...
+  /////////////////////////////
+  function worldToScreen([world_x, world_y], [camera_x, camera_y]) {
+    let i = (world_x - world_y) * tile_width_step_main;
+    let j = (world_x + world_y) * tile_height_step_main;
+    return [i + camera_x, j + camera_y];
+  }
+
+  function worldToCamera([world_x, world_y], [camera_x, camera_y]) {
+    let i = (world_x - world_y) * tile_width_step_main;
+    let j = (world_x + world_y) * tile_height_step_main;
+    return [i, j];
+  }
+
+  function tileRenderingOrder(offset) {
+    return [offset[1] - offset[0], offset[0] + offset[1]];
+  }
+
+  function screenToWorld([screen_x, screen_y], [camera_x, camera_y]) {
+    screen_x -= camera_x;
+    screen_y -= camera_y;
+    screen_x /= tile_width_step_main * 2;
+    screen_y /= tile_height_step_main * 2;
+    screen_y += 0.5;
+    return [Math.floor(screen_y + screen_x), Math.floor(screen_y - screen_x)];
+  }
+
+  function cameraToWorldOffset([camera_x, camera_y]) {
+    let world_x = camera_x / (tile_width_step_main * 2);
+    let world_y = camera_y / (tile_height_step_main * 2);
+    return { x: Math.round(world_x), y: Math.round(world_y) };
+  }
+
+  function worldOffsetToCamera([world_x, world_y]) {
+    let camera_x = world_x * (tile_width_step_main * 2);
+    let camera_y = world_y * (tile_height_step_main * 2);
+    return new p5.Vector(camera_x, camera_y);
+  }
+}
+
+// Desert - Sandy Infinite World Generator
+const s3 = (a) => {
+  /* global p5 */
+  /* exported preload, setup, draw, mouseClicked */
+
+  // Project base code provided by {amsmith,ikarth}@ucsc.edu
+
+
+  let tile_width_step_main; // A width step is half a tile's width
+  let tile_height_step_main; // A height step is half a tile's height
+
+  // Global variables. These will mostly be overwritten in setup().
+  let tile_rows, tile_columns;
+  let camera_offset;
+  let camera_velocity;
+
+  /////////////////////////////
+  // Transforms between coordinate systems
+  // These are actually slightly weirder than in full 3d...
+  /////////////////////////////
+  function worldToScreen([world_x, world_y], [camera_x, camera_y]) {
+    let i = (world_x - world_y) * tile_width_step_main;
+    let j = (world_x + world_y) * tile_height_step_main;
+    return [i + camera_x, j + camera_y];
+  }
+
+  function worldToCamera([world_x, world_y], [camera_x, camera_y]) {
+    let i = (world_x - world_y) * tile_width_step_main;
+    let j = (world_x + world_y) * tile_height_step_main;
+    return [i, j];
+  }
+
+  function tileRenderingOrder(offset) {
+    return [offset[1] - offset[0], offset[0] + offset[1]];
+  }
+
+  function screenToWorld([screen_x, screen_y], [camera_x, camera_y]) {
+    screen_x -= camera_x;
+    screen_y -= camera_y;
+    screen_x /= tile_width_step_main * 2;
+    screen_y /= tile_height_step_main * 2;
+    screen_y += 0.5;
+    return [Math.floor(screen_y + screen_x), Math.floor(screen_y - screen_x)];
+  }
+
+  function cameraToWorldOffset([camera_x, camera_y]) {
+    let world_x = camera_x / (tile_width_step_main * 2);
+    let world_y = camera_y / (tile_height_step_main * 2);
+    return { x: Math.round(world_x), y: Math.round(world_y) };
+  }
+
+  function worldOffsetToCamera([world_x, world_y]) {
+    let camera_x = world_x * (tile_width_step_main * 2);
+    let camera_y = world_y * (tile_height_step_main * 2);
+    return new p5.Vector(camera_x, camera_y);
+  }
+}
+
+// Defining Sketches
+
+let p51 = new p5(s1, "canvas-container1");
+
+let p52 = new p5(s2, "canvas-container2");
+
+let p53 = new p5(s3, "canvas-container3");
